292648ef3c954487eed627bc4a23678872b6f0f0
dcarjim0101 <dcarjim0101@g.educaand.es>
2026-05-18 13:59:05 +0200

    proyecto

--- Proyecto.java
+++ Proyecto.java
@@ -16,29 +16,44 @@ public class Proyecto {
 		System.out.println("-----------------");
 		
 		System.out.println("bienvenido, " + usuario);
+		Juegos Minecraft = new Juegos("Minecraft", "Mojang", 90, 1990, 2);
+		Juegos gta7 = new Juegos("Gta7", "Rockstar Games", 190, 1290, 6);
+		Juegos RD = new Juegos("Red Dead Redeptiom ", "BenaocazSL", 10, 280, 5);
+		Juegos CSGO = new Juegos("csgo", "Vale", 0, 3, 0);
+		Juegos RE8 = new Juegos("Resident Evil 8", "Mr.LinuxFC", 8, 8, 8);
+		Juegos SUB = new Juegos("Subnautica", "AnaODOOPutesquiCPSSH", 17, 0, 14);
 		
-		Map<String, Integer> juegos = new HashMap<>();
+		Map<Integer, Juegos> juegos = new HashMap<>();
 		
 		System.out.println("-----------------");
 		System.out.println("Lista de juegos: ");
-		juegos.put("minecraft", 1);
-		juegos.put("gta7", 2);
-		juegos.put("red dead redeptiom", 3);
-		juegos.put("csgo", 4);
-		juegos.put("resident evil 8", 5);
-		juegos.put("subnautica", 6);
+		juegos.put(1, Minecraft);
+		juegos.put(2, gta7);
+		juegos.put(3, RD);
+		juegos.put(4, CSGO);
+		juegos.put(5, RE8);
+		juegos.put(6, SUB);
 		
 		
-		Set<String> claves = juegos.keySet();
+		Set<Integer> claves = juegos.keySet();
 		mostrarJuego(juegos);
+		
 		System.out.println("-----------------");
+		System.out.println("¿Quieres comprar un juego?");
+		System.out.print(":");
+		String respuesta = sc.nextLine();
+		respuesta.toLowerCase();
+		System.out.println("-----------------");
+		if (respuesta == "si") {
+			System.out.println("¿Que juego quieres comprar?");
+			String juego = sc.nextLine();
+		}
 	}
 	
-	public static void mostrarJuego(Map<String, Integer> j) {
-		for (String n : j.keySet()) {
-			System.out.println(n);
-		}
-		
+	public static void mostrarJuego(Map<Integer, Juegos> j) {
+	    for (Integer n : j.keySet()) {
+	        System.out.println(n + " -> " + j.get(n));
+	    }
 	}
 	
 }
