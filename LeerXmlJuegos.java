0f12d1f941af8e05bbb2f5d7cd4346cbd9da2645
Antonio Hernández Gilabert <antoniohg2001@gmail.com>
2026-05-20 12:39:17 +0200

    Add files via upload

---
+++ LeerXmlJuegos.java
@@ -0,0 +1,85 @@
+package proyecto;
+
+package proyecto;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+public class LeerXmlJuegos {
+    public static ArrayList<Juegos> leerJuegos(String ruta) {
+
+        ArrayList<Juegos> lista = new ArrayList<>();
+
+        try {
+
+            File file = new File(ruta);
+
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+
+            Document documento = builder.parse(file);
+
+            documento.getDocumentElement().normalize();
+
+            NodeList listaJuegos = documento.getElementsByTagName("juego");
+
+            for (int i = 0; i < listaJuegos.getLength(); i++) {
+
+                Node nodo = listaJuegos.item(i);
+
+                if (nodo.getNodeType() == Node.ELEMENT_NODE) {
+
+                    Element juego = (Element) nodo;
+
+                    String titulo = juego
+                            .getElementsByTagName("titulo")
+                            .item(0)
+                            .getTextContent();
+
+                    String empresa = juego
+                            .getElementsByTagName("empresa_creadora")
+                            .item(0)
+                            .getTextContent();
+
+                    int precio = Integer.parseInt(
+                            juego.getElementsByTagName("precio")
+                            .item(0)
+                            .getTextContent());
+
+                    int fecha = Integer.parseInt(
+                            juego.getElementsByTagName("fecha_salida")
+                            .item(0)
+                            .getTextContent());
+
+                    int valoracion = Integer.parseInt(
+                            juego.getElementsByTagName("valoracion")
+                            .item(0)
+                            .getTextContent());
+
+                    Juegos j = new Juegos(
+                            titulo,
+                            empresa,
+                            precio,
+                            fecha,
+                            valoracion);
+
+                    lista.add(j);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return lista;
+    }
+
+}
